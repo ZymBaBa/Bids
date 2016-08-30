3fab8054661b5b56e80241c64169b2c2b97dc88f
iforamtmm <iformatmm@hotmail.com>
2016-08-30 17:44:37 +0800

    1、修改搜索框鼠标焦点效果；（Style.js） 2、删除了搜索框右边框的效果；(Style.css) 3、新增了“热门搜索”（index.html,Style.css）

--- Js/style.js
+++ Js/style.js
@@ -2,12 +2,16 @@
 $(function () {
     $("#inputBids").focusin(function () {
         this.value = "";
+        $("#inputBids").css("border", "2px solid #06c27a");
     });
     $("#inputBids").focusout(function () {
         this.value = "请输入你要查找的标底名称";
+        $("#inputBids").css("border", "2px solid #10ac86");
     });
 
 });
+
+//定义提交的按钮鼠标移入、移出、单击事件
 $(function () {
     $("input[type='submit']").mousemove(function () {
         $("input[type='submit']").css("background", "#06c27a");
