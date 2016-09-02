72d7135b11e2be7c01ad6b41ebd6d0a2f6c1ab89
iforamtmm <iformatmm@hotmail.com>
2016-09-02 17:40:07 +0800

    将要新增：发标界面

--- Js/style.js
+++ Js/style.js
@@ -10,25 +10,3 @@ $(function () {
     });
 
 });
-
-//定义提交的按钮鼠标移入、移出、单击事件
-$(function () {
-    $("input[type='submit']").mousemove(function () {
-        $("input[type='submit']").css("background", "#06c27a");
-    });
-    $("input[type='submit']").mouseout(function () {
-        $("input[type='submit']").css("background", "#10ac86");
-    });
-    $("input[type='submit']").mousedown(function () {
-        $("input[type='submit']").css("background", "#10ac86");
-    });
-    $("a[rel='next']").mousemove(function () {
-        $("a[rel='next']").css("background", "#06c27a");
-    });
-    $("a[rel='next']").mouseout(function () {
-        $("a[rel='next']").css("background", "#10ac86");
-    });
-    $("a[rel='next']").mousedown(function () {
-        $("a[rel='next']").css("background", "#10ac86");
-    });
-})
