c4a85ca3cfcbef73fc129e06996b1f3d5b9fe557
iforamtmm <iformatmm@hotmail.com>
2016-09-07 15:21:43 +0800

    使用新的CSS框架更新bidsPust.html

--- Js/style.js
+++ Js/style.js
@@ -10,3 +10,25 @@ $(function () {
     });
 
 });
+
+//bidsPust页面的物资采购和工程项目点击效果
+$(function () {
+    $("#scBids").click(function () {
+        $("#pjBids").removeClass("showCard-foucs");
+        $("#pjBids>div").addClass("pjBidsFoucs");
+        $("#pjBids>p").removeClass("pFoucs");
+        $("#scBids").addClass("showCard-foucs");
+        $("#scBids>div").removeClass("scBidsFoucs");
+        $("#scBids>p").removeClass("pFoucsD");
+        $("#ciList").html("物资采购");
+    });
+    $("#pjBids").click(function () {
+        $("#pjBids").addClass("showCard-foucs");
+        $("#pjBids>div").removeClass("pjBidsFoucs");
+        $("#pjBids>p").removeClass("pFoucsD");
+        $("#scBids").removeClass("showCard-foucs");
+        $("#scBids>div").addClass("scBidsFoucs");
+        $("#scBids>p").addClass("pFoucsD");
+        $("#ciList").html("工程项目");
+    })
+})
