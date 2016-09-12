ce26f91194a1d04378dcac64e1217da82cb75081
DESKTOP-Q96D991\Jack zhou <iformatzjy@hotmail.com>
2016-09-12 22:03:13 +0800

    1、独立了对话框出的JS 2、独立 了CSS

--- Js/open.js
+++ Js/open.js
@@ -1,3 +1,19 @@
 /**
  * Created by iform on 2016-09-12.
  */
+$(function () {
+    function ku() {
+        layer.open({
+            title:'查看报价',
+            type: 1,
+            closeBtn: 1, //不显示关闭按钮
+            shift: 2,
+            move: '.mine-move',
+            shadeClose: false, //开启遮罩关闭
+            content: $("#baoJia"),
+            area: ['600px', '300px'],
+            btn: ['yes', 'NO'],
+        });
+    }
+    $("#daKai").bind("click",ku);
+})
