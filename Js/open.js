5141316511b04263e2d205fa9ed7062b4764be96
iforamtmm <iformatmm@hotmail.com>
2016-09-14 13:44:27 +0800

    1、我的招标 Bidding.html 完成 2、我的投标 Tender.html 完成

--- Js/open.js
+++ Js/open.js
@@ -1,11 +1,14 @@
 /**
  * Created by iform on 2016-09-12.
  */
+/**
+ *@Bidding.html
+ */
 //延期招标对话框
 $(function () {
     function defer() {
         layer.open({
-            title:'延期招标',
+            title: '延期招标',
             type: 1,
             closeBtn: 0, //不显示关闭按钮
             shift: 2,
@@ -16,13 +19,14 @@ $(function () {
             btn: ['延期', '取消'],
         });
     }
-    $("#btn-defer").bind("click",defer);
+
+    $("#btn-defer").bind("click", defer);
 });
 //查看报名对话框
 $(function () {
     function apply() {
         layer.open({
-            title:'报名查看',
+            title: '报名查看',
             type: 1,
             closeBtn: 0, //不显示关闭按钮
             shift: 2,
@@ -33,13 +37,14 @@ $(function () {
             // btn: ['延期', '取消'],
         });
     }
-    $("#btn-apply").bind("click",apply);
+
+    $("#btn-apply").bind("click", apply);
 });
 //下载标书
 $(function () {
     function downBids() {
         layer.open({
-            title:'报名查看',
+            title: '报名查看',
             type: 1,
             closeBtn: 0, //不显示关闭按钮
             shift: 2,
@@ -50,21 +55,22 @@ $(function () {
             // btn: ['延期', '取消'],
         });
     }
-    $("#btn-downBids").bind("click",downBids);
+
+    $("#btn-downBids").bind("click", downBids);
 });
 //查看报价
 $(function () {
     function selectCost() {
         layer.tab({
-            type:1,
-            closeBtn:0,
-            shift:2,
-            area:'700px',
+            type: 1,
+            closeBtn: 0,
+            shift: 2,
+            area: '700px',
             move: '.mine-move',
             shadeClose: true, //开启遮罩关闭
             tab: [{
                 title: '第一轮报价',
-                content:  $('#selectCostOne').html(),
+                content: $('#selectCostOne').html(),
             }, {
                 title: '第二轮报价',
                 content: $('#selectCostTwo').html(),
@@ -74,13 +80,14 @@ $(function () {
             }],
         });
     }
-    $("#btn-selectCost").bind("click",selectCost);
+
+    $("#btn-selectCost").bind("click", selectCost);
 });
 //招标方选标
 $(function () {
     function bidsHandle() {
         layer.open({
-            title:'选标',
+            title: '选标',
             type: 1,
             closeBtn: 0, //不显示关闭按钮
             shift: 2,
@@ -91,5 +98,84 @@ $(function () {
             btn: ['稍后再选', '废标'],
         });
     }
-    $("#btn-BidsHandle").bind("click",bidsHandle);
+
+    $("#btn-BidsHandle").bind("click", bidsHandle);
 });
+/**
+ *@Tender.html
+ */
+//交标书
+$(function () {
+    function upBids() {
+        layer.open({
+            title: '交标书',
+            type: 1,
+            closeBtn: 0, //不显示关闭按钮
+            shift: 2,
+            move: '.mine-move',
+            shadeClose: true, //开启遮罩关闭
+            content: $("#upBids"),
+            area: '500px',//这样设置的话高度自增的
+            btn: ['提交', '稍后再交', '撤标'],//撤标这个动作，只有在交了标书以后出现。
+        });
+    }
+
+    $("#btn-upBids").bind("click", upBids);
+});
+
+//报价
+$(function () {
+    function supplyCost() {
+        layer.open({
+            title: '报价',
+            type: 1,
+            closeBtn: 0, //不显示关闭按钮
+            shift: 2,
+            move: '.mine-move',
+            shadeClose: true, //开启遮罩关闭
+            content: $("#supplyCost"),
+            area: '350px',//这样设置的话高度自增的
+        });
+    }
+
+    $("#btn-supplyCost").bind("click", supplyCost);
+});
+
+
+//待选标的时候查看三轮报价
+$(function () {
+    function BidsCostWait() {
+        layer.open({
+            title: '查看报价',
+            type: 1,
+            closeBtn: 0, //不显示关闭按钮
+            shift: 2,
+            move: '.mine-move',
+            shadeClose: true, //开启遮罩关闭
+            content: $("#BidsCostWait"),
+            area: '450px',//这样设置的话高度自增的
+        });
+    }
+
+    $("#btn-BidsCostWait").bind("click", BidsCostWait);
+});
+//删除
+$(function () {
+    function biddingDelete() {
+        layer.confirm('是否删除些条记录？', {
+            title:'重要提示',
+            closeBtn:0,
+            shadeClose:true,
+            btn: ['删除', '取消'] //按钮
+        }, function () {
+            layer.msg('删除成功',
+                {
+                    icon: 1,
+                    time: 1500,
+                });
+        });
+    }
+
+    $(".btn-biddingDelete").bind("click", biddingDelete);
+});
+
